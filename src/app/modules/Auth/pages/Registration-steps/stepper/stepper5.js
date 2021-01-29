bb40f3c0075b06a27be777001de007d7cab9693c
gopinath88 <gopinath@lookman.in>
2021-01-29 17:54:59 +0530

    new ui

--- src/app/modules/Auth/pages/Registration-steps/stepper/stepper5.js
+++ src/app/modules/Auth/pages/Registration-steps/stepper/stepper5.js
@@ -10,15 +10,14 @@ export default function Stepper2() {
       </span>
       <div className="d-none stepperContnet">
         <span className="stepperIcon d-flex rounded-circle justify-content-center mr-3">
-          <Image className="h-auto d-flex active" src={toAbsoluteUrl("/media/auth-screen/business_details.svg")} alt="" />
-          <Image className="h-auto d-flex stepCompleted" src={toAbsoluteUrl("/media/auth-screen/business_details_completed.svg")} alt="" />
+          <Image className="h-auto d-flex active" src={toAbsoluteUrl("/media/auth-screen/custome_link.svg")} alt="" />
         </span>
         <div className="d-flex flex-column text-left stepperRight my-auto">
           <span className="stepperSMtext">
-          Step: <text>2</text>/5 
+          Step: <text>5</text>/5 
           </span>
           <span className="stepperName">
-            Our Business
+            Link
           </span>
         </div>
       </div>        
