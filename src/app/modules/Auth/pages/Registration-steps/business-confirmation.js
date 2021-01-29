1551c98f21297b513cf1d6a5100f58dba8246ae6
gopinath88 <gopinath@lookman.in>
2021-01-29 17:36:22 +0530

    new ui

--- src/app/modules/Auth/pages/Registration-steps/business-confirmation.js
+++ src/app/modules/Auth/pages/Registration-steps/business-confirmation.js
@@ -8,8 +8,8 @@ export default function BusinessConfirmatoin() {
     <div class="busi_col">
       <div class="busi_con">
       <div className="text-left mb-6">
-      <h1 class="font-size-28 d-flex color_01234B font-weight-600 mb-10"><span class="mr-5 mt-10"> Congrats!!! Sagayaraj </span> <img src="/media/auth-screen/thumbs-up.svg" alt="thumbs-up" /></h1>      
-        <p className="font-size-24 wl_txt mr-8">https://lookman.bitrix24.com/stream/?current_fieldset= SOCSERV</p>
+      <h1 class="font-size-28 d-flex color_3F4772 font-weight-600 mb-10"><span class="mr-5 mt-10"> Congrats!!! Sagayaraj </span> <img src="/media/auth-screen/thumbs-up.svg" alt="thumbs-up" /></h1>      
+        <p className="font-size-24 wl_txt">https://lookman.bitrix24.com/stream/?current_fieldset= SOCSERV</p>
       </div>
       <div className="form-group flex-wrap flex-center">
         <button type="submit" className="btn btn-primary sign-btn h-77 font-weight-500 mt-6">
