420caef4b3d16127b39e3afc9fb80202d3ed101e
gopinath88 <gopinath@lookman.in>
2021-02-09 11:09:07 +0530

    inner - new design

--- src/app/modules/Patients-list/Patient-list.js
+++ src/app/modules/Patients-list/Patient-list.js
@@ -15,12 +15,37 @@ export default function PatientList() {
   suhbeader.setTitle("User profile");
 
   return (
-
+  <div className="pat_list">
     <div className="d-block">
       <div className="d-flex flex-row">        
         <Filter></Filter>
       </div>
-      
+      <div className="d-block mt-15">
+        <div className="d-flex">        
+            <div className="col wid_360">
+              <div className="d-flex align-items-center flex-wrap card_bg">
+                <div className="symbol symbol-50 symbol-light">
+                  <span className="symbol-label"><img src="metronic/react/demo3/media/svg/misc/006-plurk.svg" alt="" className="h-50 align-self-center" /></span>
+                </div>
+                <div className="d-flex flex-column card_txt">
+                  <a href="#" className="font-weight-bolder text-dark-75 text-hover-primary font-size-15 mb-1">Andrea Jemmiah</a>
+                  <span className="text-muted">Active</span>
+                </div>
+                <div className="d-flex flex-column mr-1 text-right font-size-12">
+                  <span className="text-muted">Nov 4, 2020</span>
+                  <span className="text-muted">10.30 AM</span>
+                </div>
+                <div className="d-flex flex-column min_20 text-right">
+                  <a class="btn btn-clean btn-hover-light-primary btn-sm btn-icon"><i class="ki ki-bold-more-ver"></i></a>
+                </div>
+              </div>
+            </div>
+            <div className="col">
+              
+            </div>
+        </div>
+      </div>
     </div>
+  </div>
   );
 }
