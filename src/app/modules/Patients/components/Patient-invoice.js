eb14436717b555fc690b6b1ba647afdb10ee7dca
gopinath88 <gopinath@lookman.in>
2021-02-17 10:28:29 +0530

    patient invoice ui

--- src/app/modules/Patients/components/Patient-invoice.js
+++ src/app/modules/Patients/components/Patient-invoice.js
@@ -7,31 +7,31 @@ export function PatientInvoice() {
   return (
     <div className="invoice_card w-100">
       <div className="text-center">
-        <h2>HMIS</h2>
-        <p>info@hmisservice.com, 9876543210</p>
+        <h2 className="font-size-24 font_weight_bold my-4">Hmis</h2>
+        <p className="font-size-16 color_707070 font_weight_bold mb-8">info@hmisservice.com, 9876543210</p>
       </div>
-      <div className="row">
+      <div className="row m-0 pb-10">
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-          <p>Doctor Treatment Invoice</p>
-          <div className="d-flex">
-            <span>Invoice Number:</span>
-            <span>012345</span>
+          <p className="font-size-18 font_weight_bold mb-6">Doctor Treatment Invoice</p>
+          <div className="d-flex mb-3">
+            <span className="font_weight_medium color_707070 mw_130">Invoice Number:</span>
+            <span className="font_weight_bold">012345</span>
           </div>
-          <div className="d-flex">
-            <span>Invoice Number:</span>
-            <span>012345</span>
+          <div className="d-flex mb-3">
+            <span className="font_weight_medium color_707070 mw_130">Billing To:</span>
+            <span className="font_weight_bold">Andrea Jemmiah</span>
           </div>
-          <div className="d-flex">
-            <span>Invoice Number:</span>
-            <span>012345</span>
+          <div className="d-flex mb-3">
+            <span className="font_weight_medium color_707070 mw_130">Client Address:</span>
+            <span className="font_weight_bold">12 Gandhi Nagar, Chennai - 600176</span>
           </div>
-          <div className="d-flex">
-            <span>Invoice Number:</span>
-            <span>012345</span>
+          <div className="d-flex mb-3">
+            <span className="font_weight_medium color_707070 mw_130">Email:</span>
+            <span className="font_weight_bold">Andrea@gmail.com</span>
           </div>
-          <div className="d-flex">
-            <span>Invoice Number:</span>
-            <span>012345</span>
+          <div className="d-flex mb-3">
+            <span className="font_weight_medium color_707070 mw_130">Due Date:</span>
+            <span className="font_weight_bold">12th Feb 2021</span>
           </div>
         </div>
         <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6">
@@ -41,8 +41,11 @@ export function PatientInvoice() {
           </div>
         </div>
       </div>
-      <Table className="table">
+      <Table className="table border-top color_292D34 font_weight_medium font-size-14">
         <thead>
+        <tr>
+            <th colSpan="6" className="font-size-16 py-3">Invoice Details</th>
+          </tr>
           <tr>
             <th>#</th>
             <th>Service</th>
@@ -67,6 +70,22 @@ export function PatientInvoice() {
             <td>4</td>
             <td>30 Mins</td>
             <td>10</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td className="color_707070">Total Cost:</td>
+            <td>100</td>
+          </tr>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td className="color_707070">Total Tax:</td>
             <td>100</td>
           </tr>
         </tbody>
