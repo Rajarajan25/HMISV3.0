491fcfd79e1d7134e3228f50523feec3dc8f3626
sivakumarlm <sivakumar2061@gmail.com>
2021-02-18 13:56:34 +0530

    adding drawer in patient profile page

--- src/app/modules/Patients/components/Patient-details-tab.js
+++ src/app/modules/Patients/components/Patient-details-tab.js
@@ -6,6 +6,19 @@ import { PatientHistory } from "./Patient-history";
 import { PatientApponiment } from "./Patient-appoinment";
 import { PatientInvoice } from "./Patient-invoice";
 
+
+import { makeStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+
+
 export  class PatientDetailsTab extends React.Component {
   constructor(props, context) {
     super(props, context);
@@ -95,7 +108,63 @@ export  class PatientDetailsTab extends React.Component {
 
 export default PatientDetailsTab;
 
+
+const useStyles = makeStyles({
+  list: {
+    width: 650,
+  },
+  fullList: {
+    width: 'auto',
+  },
+});
+
+
 export function PatientBasicDetails(){
+
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+
   return(
     <div className="d-flex flex-column patientDetailsoverview">
       <div className="d-flex flex-row patientPersoanlinfo">
@@ -184,6 +253,12 @@ export function PatientBasicDetails(){
             <span className="mt-5 font-size-12 font-weight-500 text-wrap">Heart Beat</span>
           </div>
         </div>
+      </div>
+      <div className="d-flex w-100 flex-column mt-10">
+        <Button onClick={toggleDrawer('right', true)}>Open Right</Button>
+        <Drawer className="patientProfileinfo" anchor="right" open={state.right} onClose={toggleDrawer('right', false)}>
+          <PatientInvoice />
+        </Drawer>
       </div>
     </div>
   );
