6c2fd8d1eacbbdcd044ebe6dcf0dc150c805a90a
gopinath <gopinath@lookman.in>
2021-05-20 17:31:43 +0530

    Filter Popup - 20/05/2021

--- src/app/modules/Doctors/components/Filter.js
+++ src/app/modules/Doctors/components/Filter.js
@@ -10,6 +10,9 @@ import { Link } from "react-router-dom";
 import { toAbsoluteUrl } from "../../../../_metronic/_helpers";
 //import { DropdownCustomToggler,DropdownMenu4 } from "../../../../_metronic/_partials/dropdowns";
 import Select from 'react-select';
+import { Modal } from "react-bootstrap";
+import { Dropdown } from "react-bootstrap";
+import {DropdownItemToggler} from "../../../../_metronic/_partials/dropdowns";
 
 const options = [
   { value: 'newest', label: 'Newest' },
@@ -33,6 +36,20 @@ export function Filter() {
                       <div className="w-100 d-flex">        
                         <form autoComplete="off" className="filterForm w-100">
                           <div className="d-flex">
+                            <div className="filters">
+                              <Dropdown drop="down" alignCenter className="dropdown h-100">
+                                <Dropdown.Toggle as={DropdownItemToggler} id="kt_quick_actions_search_toggle" className="h-100">
+                                  <div class="d-flex flex-wrap h-100 align-items-center justify-content-center flex-column pointer">
+                                    <div class="d-flex mt-1 mb-1 justify-content-center">
+                                      <span className="fas fa-filter filter-icon"></span>
+                                    </div>
+                                  </div>
+                                </Dropdown.Toggle>
+                                <Dropdown.Menu  className="dropdown-menu p-0 mt-1 dropdown-menu-md drop_nav">
+                                  <ServicesDropdownMenu />
+                                </Dropdown.Menu>
+                              </Dropdown>
+                            </div>
                             <div className="searchbar d-flex justify-items-center">
                               <input className="search_input" type="text" name="" placeholder="Search Staff" />
                               <a href="#" className="search_icon"><img src={toAbsoluteUrl("/media/patients/cat_search.svg")} alt="" className="" /></a>
@@ -60,4 +77,58 @@ export function Filter() {
       )}
     </>
   );
+}
+
+const duedate = [
+  { value: 'duedate', label: 'Due Date' },
+  { value: 'date', label: 'Date' },
+]
+
+const Isdate = [
+  { value: 'is', label: 'Is' },
+  { value: 'is1', label: 'Is1' },
+]
+
+const Tom = [
+  { value: 'tommorrow', label: 'Tommorrow' },
+  { value: 'today', label: 'Today' },
+]
+
+
+
+
+export function ServicesDropdownMenu() {
+  return <>
+      {/*begin::Navigation*/}
+      <ul className="navi navi-hover">
+          <li className="navi-item">
+            <p className="filter-title">Active Filters</p>
+            <div className="clearfix p-2 d-none">
+              <div className="filter-con">
+                <div className="d-flex align-items-center">
+                  <span className="p-2">Where</span>
+                  <div className="w-150 p-2">
+                    <div className="re_select">
+                      <Select options={duedate} /> 
+                    </div>
+                  </div>
+                  <div className="w-110 p-2">
+                    <div className="re_select">
+                      <Select options={Isdate} /> 
+                    </div>
+                  </div>
+                  <div className="w-250 p-2">
+                    <div className="re_select">
+                      <Select options={Tom} /> 
+                    </div>
+                  </div>
+                  <span className="p-2">X</span>
+                </div>
+              </div>
+            </div>
+          </li>
+      </ul>
+      {/*end::Navigation*/}
+
+  </>
 }
