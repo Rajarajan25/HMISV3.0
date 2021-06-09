3aba980e582e30df023d09c389d26beb55eeebbc
Maadhuri Selvaraj <maadhuri@lookman.in>
2021-06-09 20:10:46 +0530

    Service Model added

---
+++ src/app/models/ServiceModel.js
@@ -0,0 +1,17 @@
+import {TimingsModel} from './TimingsModel';
+export const ServiceModel={
+    
+
+    id:"4",
+    name:"",
+    cost:"",
+    service_type:"",
+    duration:{duration_minutes:"",buffer_before_min:"",buffer_after_min:"",pricetype:"",price:"",date_range:""},
+    payments:{retail_price:"",special_price:"",staff_name:"",duration_minutes:"",price_type:"",service_pricing_by_staff:[{
+      staff_id:"",price_type:"",retail_price:"",special_price:"",duration_minutes:"",duration_hours:""
+    }]},
+    staff_timings: [TimingsModel],
+
+  
+    
+}
