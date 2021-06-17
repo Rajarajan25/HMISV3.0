5890410e34e1907e8033d05691b34419f0b398c6
Maadhuri Selvaraj <maadhuri@lookman.in>
2021-06-17 12:51:01 +0530

    Get Service Updated

--- src/app/modules/Patients/graphql/queries.js
+++ src/app/modules/Patients/graphql/queries.js
@@ -9,25 +9,6 @@ export const GET_SERVICE = gql`
       prefered_gender
       color_code
       service_type
-      service_relationships{
-        service_duration{
-          id{
-            id
-            time_zone
-            duration_hours
-            duration_minutes
-            buffer_before_min
-          }        
-        }
-        service_staff{
-          id{
-            id
-            booking_url
-            color_code
-            display_name
-          }       
-        }
       }    
     }
-  }
   `;
