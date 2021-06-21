5a007598a3187550b80b6c2c39fb6038507c273b
gopinath <gopinath@lookman.in>
2021-06-21 18:26:50 +0530

    event - 21/06/2021

--- src/app/modules/Calendar/ResourceCell.js
+++ src/app/modules/Calendar/ResourceCell.js
@@ -5,16 +5,15 @@ class ResourceCell extends React.PureComponent {
     const { data: { color, text, data: { avatar, age, discipline } } } = this.props;
     return (
       <div className="dx-template-wrapper">
-        <div className="name" style={{ background: color }}>
-          <h2>{text}</h2>
-        </div>
-        <div className="avatar">
-          <img src={avatar} />
-        </div>
-        <div className="info" style={{ color: color }}>
-          Age: {age}
-          <br />
-          <b>{discipline}</b>
+        <div className="group-staff">
+          <div className="avatar">
+            <img src={avatar} />
+          </div>
+          <div className="infos" style={{ color: color }}>
+            <span>{text}</span>
+            <span>Age: {age}</span>
+            <span>{discipline}</span>
+          </div>
         </div>
       </div>
     );
