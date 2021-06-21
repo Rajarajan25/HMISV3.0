1de19dcbc6045f4eeb489a2f7dd9d7e28e00df8c
chandrababu vj <chandrababu@codenatives.com>
2021-06-21 18:46:39 +0530

    ui changes 21-06-2021

--- src/app/modules/Events/components/event-timing.js
+++ src/app/modules/Events/components/event-timing.js
@@ -529,8 +529,14 @@ export function Availability() {
               <div className="col-3 pr-3">
                 <div className="break_select position-relative">
                   <img src={toAbsoluteUrl("/media/patients/break_line.svg")} alt="" className="break_line" />
-                  <input type="checkbox" id="break_01" name="" className="" />
-                  <label className="" for="break_01">Break</label>
+                  <div className="hm-checkbox">
+                    <input type="checkbox" id="break_01" name="" className="" />
+                    <label className="eprice" for="break_01">
+                      <span className="circle-border"></span>
+                      <span className="chk_txt">Break</span>
+                    </label>
+                  </div>
+
                 </div>
               </div>
               <div className="col-4 p-0 d-flex">
@@ -546,8 +552,13 @@ export function Availability() {
               <div className="col-3 pr-3">
                 <div className="break_select position-relative">
                   <img src={toAbsoluteUrl("/media/patients/break_line.svg")} alt="" className="break_line" />
-                  <input type="checkbox" id="break_02" name="" className="" />
-                  <label className="" for="break_02">Break</label>
+                  <div className="hm-checkbox">
+                    <input type="checkbox" id="break_02" name="" className="" />
+                    <label className="eprice" for="break_02">
+                      <span className="circle-border"></span>
+                      <span className="chk_txt">Break</span>
+                    </label>
+                  </div>
                 </div>
               </div>
               <div className="col-4 p-0 d-flex">
@@ -564,8 +575,13 @@ export function Availability() {
             <div className="session_part row mb-2">
               <div className="col-3 pr-2 min_width100">
                 <div className="session_select">
-                  <input type="checkbox" id="session_02" name="" className="" />
-                  <label className="" for="session_02">Session 2</label>
+                  <div className="hm-checkbox">
+                    <input type="checkbox" id="session_02" name="" className="" />
+                    <label className="eprice" for="session_02">
+                      <span className="circle-border"></span>
+                      <span className="chk_txt">Session 2</span>
+                    </label>
+                  </div>
                 </div>
               </div>
               <div className="col-4 p-0 d-flex">
@@ -585,8 +601,13 @@ export function Availability() {
               <div className="col-3 pr-3">
                 <div className="break_select position-relative">
                   <img src={toAbsoluteUrl("/media/patients/break_line.svg")} alt="" className="break_line" />
-                  <input type="checkbox" id="break_03" name="" className="" />
-                  <label className="" for="break_03">Break</label>
+                  <div className="hm-checkbox">
+                    <input type="checkbox" id="break_03" name="" className="" />
+                    <label className="eprice" for="break_03">
+                      <span className="circle-border"></span>
+                      <span className="chk_txt">Break </span>
+                    </label>
+                  </div>                  
                 </div>
               </div>
               <div className="col-4 p-0 d-flex">
@@ -601,10 +622,16 @@ export function Availability() {
             <div className="break_part row mb-2">
               <div className="col-3 pr-3">
                 <div className="break_select position-relative">
-                  <img src={toAbsoluteUrl("/media/patients/break_line.svg")} alt="" className="break_line" />
-                  <input type="checkbox" id="break_04" name="" className="" />
-                  <label className="" for="break_04">Break</label>
-                </div>
+                  <img src={toAbsoluteUrl("/media/patients/break_line.svg")} alt="" className="break_line" />               
+
+                  <div className="hm-checkbox">
+                    <input type="checkbox" id="break_04" name="" className="" />
+                    <label className="eprice" for="break_04">
+                      <span className="circle-border"></span>
+                      <span className="chk_txt">Break </span>
+                    </label>
+                  </div>
+                 </div>
               </div>
               <div className="col-4 p-0 d-flex">
                 <div className="tm_area text-left pl-0">
