89d1da22546537169c6249afecce643b8101c2be
Basheer-M <82587822+Basheer-M@users.noreply.github.com>
2021-06-21 19:23:56 +0530

    Package JSON  and service model schema update
    Package JSON and service model schema update

--- src/app/modules/Patients/graphql/queries.js
+++ src/app/modules/Patients/graphql/queries.js
@@ -1,5 +1,21 @@
 import gql from "graphql-tag";
 
+const service_relationships = `
+service_relationships {
+    service_duration{
+      duration_id{
+        time_zone
+        status
+        duration_hours
+        duration_minutes
+        buffer_before_min
+        buffer_after_min
+        
+      }
+    }
+  }
+`;
+
 export const GET_SERVICE = gql`
   query{
     getService{
@@ -9,18 +25,9 @@ export const GET_SERVICE = gql`
       prefered_gender
       color_code
       service_type
-      service_relationships{
-        service_duration{
-          duration_id{
-            time_zone
-            status
-            duration_hours
-            duration_minutes
-            buffer_before_min
-            buffer_after_min
-            
-          }
-        }
-      }      }    
-    }
+      avatar_or_icon
+      avatar_or_icon_path
+    }    
+  }
   `;
+
