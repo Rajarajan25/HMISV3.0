6e005c8b542decf85352117c93691a159dc78e09
Maadhuri Selvaraj <maadhuri@lookman.in>
2021-06-25 13:48:36 +0530

    Services getApi call , Error notification, Modal for filter

--- src/app/components/Filter.js
+++ src/app/components/Filter.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useEffect } from "react";
+import React, { useEffect,useState } from "react";
 import { shallowEqual, useSelector } from "react-redux";
-import { Dropdown } from "react-bootstrap";
+import { Dropdown,Modal } from "react-bootstrap";
 import { DropdownItemToggler } from "../../_metronic/_partials/dropdowns";
 import { FilterDropDown } from './FilterDropDown';
 import Search from '../components/Search';
@@ -17,7 +17,10 @@ export function Filter(props) {
   useEffect(() => {
     return () => { };
   }, [user]);
+  const [show, setShow] = useState(false);
 
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       {user && (
@@ -29,7 +32,7 @@ export function Filter(props) {
                   <form autoComplete="off" className="filterForm w-100">
                     <div className="d-flex">
                       <div className="filters">
-                        <Dropdown drop="down" aligncenter="true" className="dropdown h-100">
+                        {/* <Dropdown drop="down" aligncenter="true" className="dropdown h-100">
                           <Dropdown.Toggle as={DropdownItemToggler} id="kt_quick_actions_search_toggle" className="h-100">
                             <div className="d-flex flex-wrap h-100 align-items-center justify-content-center flex-column pointer">
                               <div className="d-flex mt-1 mb-1 justify-content-center">
@@ -40,7 +43,17 @@ export function Filter(props) {
                           <Dropdown.Menu className="dropdown-menu p-0 mt-1 dropdown-menu-md drop_nav">
                             <FilterDropDown value={value} handleDataSource={handleDataSource} fields={fields} />
                           </Dropdown.Menu>
-                        </Dropdown>
+                        </Dropdown> */}
+                        <span className="fas fa-filter filter-icon" onClick={handleShow}></span>
+
+                        <Modal show={show} onHide={handleClose}>
+                          
+                          <Modal.Body>
+                            <FilterDropDown value={value} handleDataSource={handleDataSource} fields={fields} handleClose={handleClose} />
+
+                          </Modal.Body>
+
+                        </Modal>
                       </div>
                         <Search handleSearch={handleDataSource} data={value}></Search>
                     </div>
